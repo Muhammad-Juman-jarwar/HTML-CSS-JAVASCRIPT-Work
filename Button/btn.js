83430e3ab35c1365af4478a98e8510f76eff41a1
Muhammad Juman <muhammad1juman@gmail.com>
2022-11-05 13:56:50 -0700

    Starting coding with these small projects may these help you in your growth

---
+++ Button/btn.js
@@ -0,0 +1,19 @@
+var button =document.querySelector("button")
+button.addEventListener("click" , function(){
+    if( text.getAttribute("#id") === "bg1"){
+        text.setAttribute( "#id", "bg2");
+    }
+    }
+        
+    )
+
+// function change() {
+// 	var x = document.getElementById("bg1");
+// 	if (x.innerHTML === "OFF") {
+// 		x.innerHTML = "ON";
+// 	} else {
+// 		x.innerHTML = "OFF";
+// 	}
+// 	var element = document.body;
+// 	element.classList.toggle("lights-on");
+// } 
